5488e7c754cc917b01c1d76d1c3be60fe5b54dea
Silvio Kennecke <kennecke@heptacom.de>
2021-11-14 22:01:23 +0100

    implement mute characteristic

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,8 +1,9 @@
 import {
+  AccessoryPlugin,
   API,
   Logger,
-  AccessoryPlugin,
 } from 'homebridge';
+import storage from 'node-persist';
 
 import GPIO from 'rpi-gpio';
 import {AccessoryConfig} from 'homebridge/lib/bridgeService';
@@ -13,10 +14,16 @@ import {AccessoryConfig} from 'homebridge/lib/bridgeService';
  * parse the user config and discover/register accessories with Homebridge.
  */
 export class GpioDoorbellAccessory implements AccessoryPlugin {
+  private storage;
+
   private informationService;
   private doorbellService;
+  private muteCharacteristic;
 
   private lastRang?: number;
+
+  private readonly doorbellMuteKeyPrefix = 'homebridge-gpio-doorbell.mute.';
+  private doorbellMute = false;
 
   constructor(
     public readonly log: Logger,
@@ -24,25 +31,34 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
     public readonly api: API,
   ) {
     this.log.debug('Homebridge GPIO Doorbell loaded.');
+    this.setup();
+  }
 
-    // your accessory must have an AccessoryInformation service
+  private async setup(): Promise<void> {
+    const cacheDir = this.api.user.persistPath();
+    this.storage = storage;
+    await this.storage.init({dir: cacheDir});
+
+    // add accessory information
     this.informationService = new this.api.hap.Service.AccessoryInformation()
       .setCharacteristic(this.api.hap.Characteristic.Manufacturer, 'Homebridge')
       .setCharacteristic(this.api.hap.Characteristic.Model, 'GPIO Doorbell');
 
-    // create a new "Switch" service
+    // create new doorbell accessory
     this.doorbellService = new this.api.hap.Service.Doorbell(this.config.name);
 
-    // link methods used when getting or setting the state of the service
+    // add characteristic ProgrammableSwitchEvent
     this.doorbellService.getCharacteristic(this.api.hap.Characteristic.ProgrammableSwitchEvent);
 
+    // setup mute characteristic
+    if (this.config.enableOutput) {
+      this.muteCharacteristic = this.doorbellService.getCharacteristic(this.api.hap.Characteristic.Mute);
+      this.muteCharacteristic.onGet(this.handleMuteGet.bind(this));
+      this.muteCharacteristic.onSet(this.handleMuteSet.bind(this));
+    }
     this.setupGpio();
   }
 
-  /**
-   * REQUIRED - This must return an array of the services you want to expose.
-   * This method must be named "getServices".
-   */
   getServices() {
     return [
       this.informationService,
@@ -75,7 +91,7 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
 
     // handle GPIO output
     if (this.config.enableOutput) {
-      GPIO.write(this.config.outputGpioPin, buttonPushed, () => null);
+      GPIO.write(this.config.outputGpioPin, buttonPushed);
     }
 
     if (buttonPushed) {
@@ -96,5 +112,31 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
         this.api.hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
       );
     }
+  }
+
+  /**
+   * @param value
+   * @private
+   */
+  private async handleMuteSet(value: boolean): Promise<void> {
+    this.doorbellMute = value;
+    await this.storage.setItem(this.doorbellMuteKeyPrefix + this.config.name, this.doorbellMute);
+
+    if (!this.doorbellMute && this.config.enableOutput) {
+      GPIO.write(this.config.outputGpioPin, false);
+    }
+  }
+
+  /**
+   * @private
+   */
+  private async handleMuteGet(): Promise<boolean> {
+    if (this.doorbellMute === null) {
+      const persisted = await this.storage.getItem(this.doorbellMuteKeyPrefix + this.config.name);
+
+      await this.handleMuteSet(!!persisted);
+    }
+
+    return this.doorbellMute;
   }
 }
