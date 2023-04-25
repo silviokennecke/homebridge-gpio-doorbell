873aa3a87a5190cfcad79948f55143c12150b6ac
Jorianv <94118957+Jorianv@users.noreply.github.com>
2023-04-25 08:51:20 +0200

    Update accessory.ts
    Correct amount of whitespaces

--- src/accessory.ts
+++ src/accessory.ts
@@ -64,9 +64,8 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
 
     // setup gpio
     this.setupGpio();
-    
     // Default debounce delay is 50ms
-    this.debounceDelay = this.config.debounceDelay || 50; 
+    this.debounceDelay = this.config.debounceDelay || 50;
 
   }
 
@@ -94,65 +93,65 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
    * @private
    */
   private async handlePinChange(gpioPin: number, circuitOpen: boolean): Promise<void> {
-   this.log.debug(`Pin ${gpioPin} changed state to ${circuitOpen}.`);
+    this.log.debug(`Pin ${gpioPin} changed state to ${circuitOpen}.`);
 
-   let buttonPushed = !circuitOpen;
+    let buttonPushed = !circuitOpen;
 
-   if (this.config.negateInput) {
-    buttonPushed = !buttonPushed;
-   }
-
-   // Debounce logic
-   if (buttonPushed) {
-     if (this.debounceTimeout) {
-      clearTimeout(this.debounceTimeout);
+    if (this.config.negateInput) {
+      buttonPushed = !buttonPushed;
     }
 
-    this.debounceTimeout = setTimeout(async () => {
+    // Debounce logic
+    if (buttonPushed) {
+      if (this.debounceTimeout) {
+        clearTimeout(this.debounceTimeout);
+      }
+
+      this.debounceTimeout = setTimeout(async () => {
       // handle GPIO output
-      if (this.config.enableOutput && !this.doorbellMute) {
-        this.log.debug(`Setting GPIO pin ${this.config.outputGpioPin} to ${buttonPushed ? 'HIGH' : 'LOW'}`);
+        if (this.config.enableOutput && !this.doorbellMute) {
+          this.log.debug(`Setting GPIO pin ${this.config.outputGpioPin} to ${buttonPushed ? 'HIGH' : 'LOW'}`);
 
-        GPIO.write(this.config.outputGpioPin, buttonPushed);
-      }
+          GPIO.write(this.config.outputGpioPin, buttonPushed);
+        }
 
-      // handle throttle time
-      const now = Date.now();
-      if (this.lastRang && (this.lastRang + this.config.throttleTime) >= now) {
-        this.log.debug(`Ignoring state change on pin ${gpioPin} because throttle time has not expired.`);
-        return;
-      } else {
-        this.lastRang = Date.now();
-      }
+        // handle throttle time
+        const now = Date.now();
+        if(this.lastRang && (this.lastRang + this.config.throttleTime) >= now) {
+          this.log.debug(`Ignoring state change on pin ${gpioPin} because throttle time has not expired.`);
+          return;
+        } else {
+          this.lastRang = Date.now();
+        }
 
-      // forward ring to homekit
-      this.log.info(`Doorbell "${this.config.name}" rang.`);
+        // forward ring to homekit
+        this.log.info(`Doorbell "${this.config.name}" rang.`);
 
-      if (!this.config.enableHttpTrigger || !this.config.httpTriggerUrl) {
+        if (!this.config.enableHttpTrigger || !this.config.httpTriggerUrl) {
         // ring in homekit
-        this.log.info('Forwarding ring directly to HomeKit.');
-        this.doorbellService.updateCharacteristic(
-          this.api.hap.Characteristic.ProgrammableSwitchEvent,
-          this.api.hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-        );
-      } else {
+          this.log.info('Forwarding ring directly to HomeKit.');
+          this.doorbellService.updateCharacteristic(
+            this.api.hap.Characteristic.ProgrammableSwitchEvent,
+            this.api.hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+          );
+        } else {
         // ring via 3rd party plugin
-        const url = this.config.httpTriggerUrl;
-        this.log.info(`Performing request to webhook at ${url}.`);
-        try {
-          await axios.get(url);
-        /* eslint-disable  @typescript-eslint/no-explicit-any */
-        } catch (e: any) {
-          if (e.response) {
-            this.log.error(`Request to webhook failed with status code ${e.response.status}: ${e.response.data}`);
-          } else {
-            this.log.error(`Request to webhook failed with message: ${e?.message}`);
+          const url = this.config.httpTriggerUrl;
+          this.log.info(`Performing request to webhook at ${url}.`);
+          try {
+            await axios.get(url);
+            /* eslint-disable  @typescript-eslint/no-explicit-any */
+          } catch (e: any) {
+            if (e.response) {
+              this.log.error(`Request to webhook failed with status code ${e.response.status}: ${e.response.data}`);
+            } else {
+              this.log.error(`Request to webhook failed with message: ${e?.message}`);
+            }
           }
         }
-      }
-    }, this.debounceDelay); // Use the debounce delay from the user's config
+      }, this.debounceDelay); // Use the debounce delay from the user's config
+    }
   }
-}
 
 
   private handleMuteSet(value: boolean): void {
